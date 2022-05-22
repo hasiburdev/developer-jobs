2ca775fca38a861e51f7dca32f69a257dda6727a
Hasibur Rahman <hasiburdev@gmail.com>
2022-05-22 08:33:58 +0600

    Update Data Location

--- js/main.js
+++ js/main.js
@@ -1,32 +1,30 @@
-const findJobsBtn = document.querySelector('.button-container')
+const findJobsBtn = document.querySelector(".button-container");
 
+findJobsBtn.addEventListener("click", () => {
+  let searchText = document.querySelector("#filter-jobs").value;
+  let searchPlace = document.querySelector("#filter-places").value;
+  let searchCompany = document
+    .querySelector("#filter-company")
+    .value.toString();
 
-findJobsBtn.addEventListener('click', () => {
-    let searchText = document.querySelector('#filter-jobs').value
-    let searchPlace = document.querySelector('#filter-places').value
-    let searchCompany = document.querySelector('#filter-company').value.toString()
-
-
-
-    getJobs().then(jobs => {
-        let filteredJobs = filterJobs(jobs, searchText, searchPlace,searchCompany)
-        showJobs(filteredJobs)
-    })
-})
+  getJobs().then((jobs) => {
+    let filteredJobs = filterJobs(jobs, searchText, searchPlace, searchCompany);
+    showJobs(filteredJobs);
+  });
+});
 const getJobs = async () => {
-    return fetch('https://hr-hasan.github.io/developer-jobs/data/data.json')
-    .then(response => response.json())
-    .then(data => {
-        return data
-    })
-}
+  return fetch("./data/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+};
 
 function showJobs(jobs) {
-    const jobsContainer = document.querySelector('.jobs-container') 
-    let htmlJobs = ''
-    jobs.forEach(job => {
-
-        htmlJobs += `<div class="job-tile">
+  const jobsContainer = document.querySelector(".jobs-container");
+  let htmlJobs = "";
+  jobs.forEach((job) => {
+    htmlJobs += `<div class="job-tile">
         <div class="top">
             <img src="${job.logo}" alt="">
             <span class="material-icons more_horiz">more_horiz</span>
@@ -51,48 +49,49 @@ function showJobs(jobs) {
             </div>
         </div>
     </div>
-    `
-    })
-    jobsContainer.innerHTML = htmlJobs
+    `;
+  });
+  jobsContainer.innerHTML = htmlJobs;
 }
 
-function filterJobs(jobs, searchText, searchPlace,searchCompany) {
-    let filteredJobs = []
-    if(searchText || searchCompany || searchPlace) {
-        filteredJobs = jobs.filter(job => {
-            if(searchText) {
-                if(job.roleName.toLowerCase().includes(searchText)
-                || job.type.toLowerCase().includes(searchText)
-                || job.requirements.content.toLowerCase().includes(searchText)
-                ) {
-                    return true
-                }
-            }
-            else if (searchPlace && job.location.toLowerCase().includes(searchPlace)){
-                return true
-            }
-            else if(searchCompany && job.company.toLowerCase().includes(searchCompany)) {
-                return true
-            }
-            else {
-                return false
-            }
-
-        })
-    } 
-    else {
-        filteredJobs = jobs
-    }
-    return filteredJobs
+function filterJobs(jobs, searchText, searchPlace, searchCompany) {
+  let filteredJobs = [];
+  if (searchText || searchCompany || searchPlace) {
+    filteredJobs = jobs.filter((job) => {
+      if (searchText) {
+        if (
+          job.roleName.toLowerCase().includes(searchText) ||
+          job.type.toLowerCase().includes(searchText) ||
+          job.requirements.content.toLowerCase().includes(searchText)
+        ) {
+          return true;
+        }
+      } else if (
+        searchPlace &&
+        job.location.toLowerCase().includes(searchPlace)
+      ) {
+        return true;
+      } else if (
+        searchCompany &&
+        job.company.toLowerCase().includes(searchCompany)
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  } else {
+    filteredJobs = jobs;
+  }
+  return filteredJobs;
 }
-
 
 function toggleDarkLight() {
-    var body = document.getElementById("body");
-    var currentClass = body.className;
-    body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
-  }
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+}
 
-getJobs().then(data => {
-    showJobs(data)
-})
+getJobs().then((data) => {
+  showJobs(data);
+});
